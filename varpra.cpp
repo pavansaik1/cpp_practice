f1534b548e204a90498da6843dede2bfdb48af5c
pavansaik1 <pavansaikandregula48845@gmail.com>
2025-01-29 21:39:15 +0530

    practicing variables

---
+++ varpra.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"my name is pavansai"<<endl;
+    cout<<"pavansai is 19 years old"<<endl;
+    cout<<"19 means youngstar"<<endl;
+    cout<<"pavansai is youngstar"<<endl;
+    // practise the real one
+    string charactername = "kps";
+    int characterage = 19;
+    cout<<"my name is "<<charactername<<endl;
+    cout<<""<<charactername << " is 19 years old"<<endl;
+    cout<<""<<characterage << "means youngstar"<<endl;
+    cout<<"" <<charactername << " is youngstar"<<endl;
+
+    return 0;
+}
