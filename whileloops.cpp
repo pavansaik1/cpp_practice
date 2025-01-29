c52494f0c14debcf77a123c28827e4911bc12406
pavansaik1 <pavansaikandregula48845@gmail.com>
2025-01-29 21:56:04 +0530

    while loops

---
+++ whileloops.cpp
@@ -0,0 +1,21 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int num = 5;
+//     while(num < 10){
+//         cout << num <<endl;
+//         num++;
+//     }
+//     return 0;
+// }>>gives 5,6,7,8,9
+#include <iostream>
+using namespace std;
+ int main(){
+    int num = 10;
+    do{
+        cout << num <<endl;
+        num++;
+    }while(num < 10);
+   return 0;
+
+ }
