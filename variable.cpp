19b8dd8c4f26d4dee769485cfccb87a32855ce87
pavansaik1 <pavansaikandregula48845@gmail.com>
2025-01-29 21:37:06 +0530

    how to use variables

---
+++ variable.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //variables are used to store..
+    cout<<"hello hari" <<endl;
+    cout<<"hari age 25 only" << endl;
+    
+    //now using variables
+    string charactername = "Hari";
+    int characterage = 15;
+    cout<<"hello "<<charactername<<endl; //when u are using variable,u must add <<
+    
+
+
+    return 0;
+}
