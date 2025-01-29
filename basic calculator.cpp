434a7237c8db583e041194ed5a2cecf14c582fd9
pavansaik1 <pavansaikandregula48845@gmail.com>
2025-01-29 21:46:32 +0530

    building a basic calculator with user input

---
+++ basic calculator.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float num1,num2;
+    cout<<"enter number one:"<<endl;
+    cin>>num1;
+    cout<<"enter number two:"<<endl;
+    cin>>num2;
+    cout<<"" << num1 + num2 << "is the final answer" << endl;
+    return 0;
+}
