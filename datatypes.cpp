67a6aaae6d814ab4e8e5668230c3968792857a31
pavansaik1 <pavansaikandregula48845@gmail.com>
2025-01-29 21:40:25 +0530

    datatypes

---
+++ datatypes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //types of variables
+    char grade ='a';
+    string name = "kps";
+    float decimalnum = 2.5;
+    int num = 9;
+    bool ismale=true;
+
+    cout << ismale << endl;
+    return 0; 
+
+
+}
