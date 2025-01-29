1280a7d89ff2e8b402d718c1de5b113b01373919
pavansaik1 <pavansaikandregula48845@gmail.com>
2025-01-29 21:45:04 +0530

    getting an user input

---
+++ gettinguserinput.cpp
@@ -0,0 +1,15 @@
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     char grade;
+//     cout << "enter your grade\n";
+//     cin>>grade;
+//     cout<<"your grade is "<< grade <<" in maths\n";
+//     string name;
+//     cout<<"enter your name:\n"<<endl;
+//     getline(cin,name);
+//     cout<<"YOur name is " << name <<endl;
+//     return 0;
+// }
+
