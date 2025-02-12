ad3297788a996e8dcc7059fcdd118125466b8765
pavansaik1 <pavansaikandregula48845@gmail.com>
2025-02-12 15:05:45 +0530

    deque in stl

---
+++ deque and list.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+#include<list>
+#include<deque>
+int main(){
+    deque<int>d={1,2,3,4,5,6,7,8,9,10};
+    cout<<d[0];
+    //deque is as same as list,but in list u cannot find index number,
+    //meanwhile in deque u can...
+}
